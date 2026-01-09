95164b4fff5d2661cdd078159cb9b27ca9ce2cd5
Zavier Ferodova <zavierferodova@gmail.com>
2026-01-09 15:01:59 +0700

    Update TransactionCouponOutputSerializer on column item_discount_percentage to integer field

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -15,7 +15,7 @@ class TransactionCouponOutputSerializer(serializers.ModelSerializer):
     code = serializers.CharField(source='coupon_code.code')
     name = serializers.CharField(source='coupon_code.coupon.name')
     type = serializers.CharField(source='coupon_code.coupon.type')
-    item_discount_percentage = serializers.CharField(source='coupon_code.coupon.discount_percentage', read_only=True)
+    item_discount_percentage = serializers.IntegerField(source='coupon_code.coupon.discount_percentage', read_only=True)
 
     class Meta:
         model = TransactionCoupon
